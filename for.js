0ef36c33d084b1392fb57a0b3a4d1f719cb913f7
Yosuf Bhuiyan <yosufbhuiyanbusiness@gmail.com>
2023-01-09 23:38:19 +0600

    first commit

---
+++ for.js
@@ -0,0 +1,17 @@
+for (var step = 0 ; step < 10 ; step++){
+  console.log('I am Yosuf');
+}
+
+
+
+console.log('--------------------------------0-----------------------')
+
+
+
+let age = 18;
+
+if(age < 18){
+    console.log('You are allowed')
+} else(
+    console.log('You are not allowed')
+)
